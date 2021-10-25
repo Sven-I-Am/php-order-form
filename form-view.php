84cc2894557ea1c43e2fc81e50646910c586db1c
Sven <sven.vander.mierde@gmail.com>
2021-10-25 17:33:29 +0200

    basic functinality implemented

---
+++ form-view.php
@@ -0,0 +1,119 @@
+<!doctype html>
+<?php
+if (isset($_COOKIE[COOKIE_NAME]))
+{
+$totalSpentValue = $_COOKIE[COOKIE_NAME] + $totalValue;
+}
+else
+{
+$totalSpentValue += $totalValue;
+}
+setCookie(COOKIE_NAME, $totalSpentValue, time() + (86400 * 30), "/");
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" type="text/css"
+          rel="stylesheet"/>
+    <title>Order food & drinks</title>
+</head>
+<body>
+
+<div class="container">
+    <h1>Order food in restaurant "the Personal Ham Processors"</h1>
+    <nav>
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="?food=1">Order food</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?food=0">Order drinks</a>
+            </li>
+        </ul>
+    </nav>
+    <?php
+    if($emailError!=''){
+        echo '<div class="alert alert-danger">' . $emailError . '</div>';
+    }
+    if($streetError!=''){
+        echo '<div class="alert alert-danger">' . $streetError . '</div>';
+    }
+    if ($strNumberError != '') {
+        echo '<div class="alert alert-danger">' . $strNumberError . '</div>';
+    }
+    if ($cityError != '') {
+        echo '<div class="alert alert-danger">' . $cityError . '</div>';
+    }
+    if ($zipcodeError != '') {
+        echo '<div class="alert alert-danger">' . $zipcodeError . '</div>';
+    }
+    if ($orderError !=''){
+        echo '<div class="alert alert-danger">' . $orderError . '</div>';
+    }
+    if ($success!=''){
+        echo '<div class="alert alert-success">' . $success . '</div>';
+    }
+    ?>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="email">E-mail:</label>
+                <input type="text" id="email" name="email" class="form-control" value="<?php if($_SESSION["email"]!=''){echo $_SESSION["email"];} ?>"<?php if($emailError!=''){echo 'style="border-color: #f5c6cb; background-color: #f8d7da"';}elseif($_SESSION["email"]!=''){echo 'style="border-color: #c3e6cb; background-color: #d4edda"';} ?>/>
+            </div>
+            <div></div>
+        </div>
+        <fieldset>
+            <legend>Address</legend>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="street">Street:</label>
+                    <input type="text" name="street" id="street" class="form-control" value="<?php if($_SESSION["street"]!=''){echo $_SESSION["street"];} ?>"<?php if($streetError!=''){echo 'style="border-color: #f5c6cb; background-color: #f8d7da"';}elseif($_SESSION["street"]!=''){echo 'style="border-color: #c3e6cb; background-color: #d4edda"';} ?>>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="streetnumber">Street number:</label>
+                    <input type="text" id="streetnumber" name="streetnumber" class="form-control" value="<?php if($_SESSION["strNumber"]!=''){echo $_SESSION["strNumber"];} ?>"<?php if($strNumberError!=''){echo 'style="border-color: #f5c6cb; background-color: #f8d7da"';}elseif($_SESSION["strNumber"]!=''){echo 'style="border-color: #c3e6cb; background-color: #d4edda"';} ?>>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="city">City:</label>
+                    <input type="text" id="city" name="city" class="form-control" value="<?php if($_SESSION["city"]!=''){echo $_SESSION["city"];} ?>"<?php if($cityError!=''){echo 'style="border-color: #f5c6cb; background-color: #f8d7da"';}elseif($_SESSION["city"]!=''){echo 'style="border-color: #c3e6cb; background-color: #d4edda"';} ?>>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="zipcode">Zipcode</label>
+                    <input type="text" id="zipcode" name="zipcode" class="form-control" value="<?php if($_SESSION["zipcode"]!=''){echo $_SESSION["zipcode"];} ?>"<?php if($zipcodeError!=''){echo 'style="border-color: #f5c6cb; background-color: #f8d7da"';}elseif($_SESSION["zipcode"]!=''){echo 'style="border-color: #c3e6cb; background-color: #d4edda"';} ?>>
+                </div>
+            </div>
+        </fieldset>
+
+        <fieldset>
+            <legend>Products</legend>
+            <?php foreach ($products AS $i => $product): ?>
+                <label>
+                    <input type="checkbox" value="<?php echo $product['price']; ?>" name="products[]"/> <?php echo $product['name'] ?> -
+                    &euro; <?php echo number_format($product['price'], 2) ?></label><br />
+            <?php endforeach; ?>
+        </fieldset>
+        
+        <label>
+            <input type="checkbox" name="express_delivery" value="5" />
+            Express delivery (+ 5 EUR) 
+        </label>
+            
+        <button type="submit" class="btn btn-primary">Order!</button>
+    </form>
+    <div>Total for current order: <strong>&euro; <?php //echo $orderValue; ?></strong></div>
+
+    <footer>In the last 30 days you've ordered <strong>&euro; <?php echo $totalSpentValue; ?></strong> in food and drinks.</footer>
+</div>
+
+<style>
+    footer {
+        text-align: center;
+    }
+</style>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,150 @@
+<?php
+//this line makes PHP behave in a more strict way
+declare(strict_types=1);
+
+//we are going to use session variables, so we need to enable sessions
+session_start();
+
+//your products with their price.
+if(!isset($_GET["food"]) || $_GET["food"]!=0){
+    $products = [
+        ['name' => 'Club Ham', 'price' => 3.20],
+        ['name' => 'Club Cheese', 'price' => 3],
+        ['name' => 'Club Cheese & Ham', 'price' => 4],
+        ['name' => 'Club Chicken', 'price' => 4],
+        ['name' => 'Club Salmon', 'price' => 5]
+    ];
+} else {
+    $products = [
+        ['name' => 'Cola', 'price' => 2],
+        ['name' => 'Fanta', 'price' => 2],
+        ['name' => 'Sprite', 'price' => 2],
+        ['name' => 'Ice-tea', 'price' => 3],
+    ];
+}
+const COOKIE_NAME = "totalSpent";
+$totalValue = 0;
+$totalSpentValue = 0;
+define("REST_EMAIL", "sven.vander.mierde@gmail.com");
+
+//check and validate email
+function validateEmail($email) : string
+{
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $_SESSION["email"] = $email;
+        $emailError = '';
+    } else {
+        $emailError = 'Please enter a valid email';
+    }
+    return $emailError;
+}
+
+function validateStreet($street): string
+{
+    if (strlen($street)>=3 && strlen($street)<=44 && preg_match("/[0-9]/", $street) == 0){
+        $_SESSION["street"] = $street;
+        $streetError = '';
+    }else{
+        $streetError = 'Please enter a valid street name';
+    }
+    return $streetError;
+}
+
+function validateStreetNumber($number): string
+{
+    if (strlen($number)!=0 && preg_match("/[^0-9]/", $number) == 0){
+        $_SESSION["strNumber"] = $number;
+        $strNumberError = '';
+    } else {
+        $strNumberError = 'Please enter a valid streetnumber, only numbers are allowed!';
+    }
+    return $strNumberError;
+}
+
+function validateCity($city): string
+{
+    if (strlen($city)>=2 && strlen($city)<=26 && preg_match("/[0-9]/", $city) == 0){
+        $_SESSION["city"] = $city;
+        $cityError = '';
+    }else{
+        $cityError = 'Please enter a valid street name';
+    }
+    return $cityError;
+}
+
+function validateZipcode($zip): string
+{
+    $zip = (int)$zip;
+    if ($zip >= 1000&& $zip<=9999){
+        $_SESSION["zipcode"] = $zip;
+        $zipcodeError = '';
+    } else {
+        $zipcodeError = 'Please enter a valid zipcode: between 1000 and 9999';
+    }
+    return $zipcodeError;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $error = 0;
+    $email = $_POST["email"];
+    $emailError = validateEmail($email);
+    if($emailError!=''){$error++;}
+    $street = $_POST["street"];
+    $streetError = validateStreet($street);
+    if($streetError!=''){$error++;}
+    $strNumber = $_POST["streetnumber"];
+    $strNumberError = validateStreetNumber($strNumber);
+    if($strNumberError!=''){$error++;}
+    $city = $_POST["city"];
+    $cityError = validateCity($city);
+    if($cityError!=''){$error++;}
+    $zipcode = $_POST["zipcode"];
+    $zipcodeError = validateZipcode($zipcode);
+    if($zipcodeError!=''){$error++;}
+    if(!empty($_POST['products'])) {
+        foreach($_POST['products'] as $product) {
+            $price = $product;
+            $price = (float)$price;
+            $totalValue += $price;
+        }
+    } else {
+        $error++;
+        $orderError = "please select products to order";
+    }
+    if(isset($_POST["express_delivery"])){
+        $deliveryTime = '45 minutes';
+        $price = $_POST["express_delivery"];
+        $price = (float)$price;
+        $totalValue += $price;
+        $express = true;
+    } else {
+        $deliveryTime = '2 hours';
+        $express = false;
+    }
+
+    if ($error == 0){
+
+        $mailToUser = "Thank you for ordering with us!\nDelivery adress: \n" . $street . " " . $strNumber. "\n Zipcode: " . $zipcode . " - City: " . $city . "\n Order cost: " . $totalValue . "\n Deliverytime: " . $deliveryTime . "\n Exress: " . $express;
+        $mailToOwner = "We got an order in!\n" . $mailToUser;
+        mail(REST_EMAIL, "new order!", $mailToOwner);
+        mail($email, "We got your order", $mailToUser);
+        $success = 'Order sent successfully. Check your inbox!';
+    }
+}
+
+function whatIsHappening() {
+    echo '<h2>$_GET</h2>';
+    var_dump($_GET);
+    echo '<h2>$_POST</h2>';
+    var_dump($_POST);
+    echo '<h2>$_COOKIE</h2>';
+    var_dump($_COOKIE);
+    echo '<h2>$_SESSION</h2>';
+    var_dump($_SESSION);
+}
+
+whatIsHappening();
+
+
+
+require 'form-view.php';
